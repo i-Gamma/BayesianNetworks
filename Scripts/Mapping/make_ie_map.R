93c04a50335c8d2580cb2c2868119b274517978c
MiguelEq <equihuam@gmail.com>
2015-09-04 15:45:40 -0500

    Script with function to adapt to different machines

--- Scripts/Mapping/make_ie_map.R
+++ Scripts/Mapping/make_ie_map.R
@@ -3,48 +3,54 @@ library("raster")
 library("rgdal")
 
 machine_set_up <- function ()
-{ users <- dir("c:/users")
-  if grepl("miguel.equihua", users)
+{
+  users <- dir("c:/users")
+  if ("miguel.equihua" %in% users)
   {
     equipo <- "escritorio_miguel"
+    # set working directory
+    dir_maps <- "C:/Users/miguel.equihua/Documents/1 Nube/Dropbox/Datos Redes Bayesianas/mapas/"
+    dir_work <- gsub("mapas", "Datos_para_mapeo", dir_maps)
+    setwd(dir_work)
   }
-  return equipo
+  
+  return (dir_maps)
 }
 
+dir_maps <- machine_set_up()
 
-# set working directory
-setwd("C:/Users/miguel.equihua/Documents/1 Nube/Dropbox/Datos Redes Bayesianas/Datos_para_mapeo")
+
 
 # load train data
-train_data = read.table("bn_ie_tabfinal_20150830.csv",sep=",",header=TRUE)
+train_data <- read.table("bn_ie_tabfinal_20150830.csv",sep=",",header=TRUE)
 
 # head of train data
 head(train_data)
 
 # load BN prediction
-bn_output = read.table("IE_Naive_Step_n5_zvh31_peinada_sin_medias_oct2_EMBB - copia.txt",sep=",",header=TRUE)
+bn_output <- read.table("IE_Naive_Step_n5_zvh31_peinada_sin_medias_oct2_EMBB - copia.txt",sep=",",header=TRUE)
 length(bn_output[,1])
 
 # ie
-ie = (max(bn_output[,1])-bn_output[,1])/(max(bn_output[,1]))
+ie <- (max(bn_output[,1])-bn_output[,1])/(max(bn_output[,1]))
 head(ie)
 
 
 # ie map
 ie_map_df = data.frame(x=train_data$x, y=train_data$y, ie=ie)
-coordinates(ie_map_df)=~x+y
-gridded(ie_map_df)=TRUE
-ie_raster = raster(ie_map_df)
+coordinates(ie_map_df) <- ~ie_map_df$x + ie_map_df$y
+gridded(ie_map_df) <- TRUE
+ie_raster <- raster(ie_map_df)
 
 # load base raster
-base = raster("bov_cbz_km2.tif")
+base <- raster("bov_cbz_km2.tif")
 
 # set projection
-projection(ie_raster)=projection(base)
+projection(ie_raster) <- projection(base)
 plot (ie_raster)
 
 # write to disk
-dir_maps <- "C:/Users/miguel.equihua/Documents/1 Nube/Dropbox/Datos Redes Bayesianas/mapas/"
+
 tif_file <- paste(dir_maps, "IE_Naive_Step_n5_zvh31_peinada_sin_medias_oct2_EMBB - copia.txt.tif", sep="")
 ie <- writeRaster(ie_raster, filename=tif_file, format="GTiff", overwrite=TRUE)
 
