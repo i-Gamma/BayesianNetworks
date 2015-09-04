ea27a3625a12c24fe57ddb130ed4a6069315158c
MiguelEq <equihuam@gmail.com>
2015-09-04 14:32:31 -0500

    Update make_ie_map.R

--- Scripts/Mapping/make_ie_map.R
+++ Scripts/Mapping/make_ie_map.R
@@ -1,10 +1,19 @@
-
 # packages
 library("raster")
 library("rgdal")
 
+machine_set_up <- function ()
+{ users <- dir("c:/users")
+  if grepl("miguel.equihua", users)
+  {
+    equipo <- "escritorio_miguel"
+  }
+  return equipo
+}
+
+
 # set working directory
-setwd("E:/data/Miguel/Integridad")
+setwd("C:/Users/miguel.equihua/Documents/1 Nube/Dropbox/Datos Redes Bayesianas/Datos_para_mapeo")
 
 # load train data
 train_data = read.table("bn_ie_tabfinal_20150830.csv",sep=",",header=TRUE)
@@ -13,7 +22,7 @@ train_data = read.table("bn_ie_tabfinal_20150830.csv",sep=",",header=TRUE)
 head(train_data)
 
 # load BN prediction
-bn_output = read.table("IE_casi_final_oct3Cd_lat15_TAN.txt",sep=",",header=TRUE)
+bn_output = read.table("IE_Naive_Step_n5_zvh31_peinada_sin_medias_oct2_EMBB - copia.txt",sep=",",header=TRUE)
 length(bn_output[,1])
 
 # ie
@@ -22,17 +31,20 @@ head(ie)
 
 
 # ie map
-ie_map_df = data.frame(x=train_data$x,y=train_data$y,ie=ie)
+ie_map_df = data.frame(x=train_data$x, y=train_data$y, ie=ie)
 coordinates(ie_map_df)=~x+y
 gridded(ie_map_df)=TRUE
 ie_raster = raster(ie_map_df)
 
 # load base raster
-base = raster("E:/data/base/bov_cbz_km2.tif")
+base = raster("bov_cbz_km2.tif")
 
 # set projection
 projection(ie_raster)=projection(base)
+plot (ie_raster)
 
 # write to disk
-ie <- writeRaster(ie_raster, filename="IE_casi_final_oct3Cd_lat15_TAN.tif", format="GTiff", overwrite=TRUE)
+dir_maps <- "C:/Users/miguel.equihua/Documents/1 Nube/Dropbox/Datos Redes Bayesianas/mapas/"
+tif_file <- paste(dir_maps, "IE_Naive_Step_n5_zvh31_peinada_sin_medias_oct2_EMBB - copia.txt.tif", sep="")
+ie <- writeRaster(ie_raster, filename=tif_file, format="GTiff", overwrite=TRUE)
 
