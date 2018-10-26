4829705bb04795f4492a6e3a5fcf5ba6977a31b0
MiguelEq <equihuam@gmail.com>
2018-10-26 13:47:21 -0500

    Add trend plots

--- Scripts/Mapping/make_ie_map_veracruz.R
+++ Scripts/Mapping/make_ie_map_veracruz.R
@@ -4,7 +4,7 @@ library("rgdal")
 library("R.utils")
 library("data.table")
 
-# Utiliza el comnado de DOS para mapear una url a un disco local virtual.
+# Utiliza el comando "net" de DOS para mapear una url a un disco local virtual.
 url <- "https://inecol.sharepoint.com/sites/ie-2016/Base_de_datos_IE/3_vars_explicativas/Formato_tabular"
 if (!dir.exists("Z:/")) system(paste("net use Z:", url, sep = " "))
 
@@ -13,7 +13,7 @@ list.files(dir_datos_bn)
 datos_bn <- paste(dir_datos_bn, "2_set_cobertura_completa/ie_time_series.csv", sep="")
 
 # load data
-data <- fread(datos_bn)
+data <- fread(datos_bn, stringsAsFactors = FALSE, showProgress = TRUE)
 data_veracruz <- subset(data, subset = state_name == "VERACRUZ DE IGNACIO DE LA LLAVE") 
 data_veracruz <- data_veracruz[complete.cases(data_veracruz),]
 
@@ -29,19 +29,25 @@ plot (base)
 mapas <- names(data_veracruz)[c(3,6:16)]
 for (mapa in mapas)
 {
-  ie <- data_veracruz[,..mapa]
+  if (mapa == "zvh_31")
+    ie <- data_veracruz[,..mapa]
+  else
+    dato <- data_veracruz[,..mapa] * 100
+  
   # ie map
-  ie_map_df <- data.frame(x=data_veracruz$x, y=data_veracruz$y, ie=ie * 100)
-  coordinates(ie_map_df) <- ~ x + y
+  map_df <- data.frame(x=data_veracruz$x, y=data_veracruz$y, dato)
+  coordinates(map_df) <- ~ x + y
   gridded(ie_map_df) <- TRUE
-  ie_raster <- raster(ie_map_df)
+  raster_map <- raster(map_df)
   
   # set projection
-  projection(ie_raster) <- projection(base)
-  plot (ie_raster, axes=FALSE, box=FALSE, main = mapa)
+  projection(raster_map) <- projection(base)
+  plot (raster_map, axes=FALSE, box=FALSE, main = mapa)
   
   # write to disk
   tif_file <- paste(dir_datos_bn, "veracruz/ver_",  mapa, ".tif", sep="")
-  ie <- writeRaster(ie_raster, filename=tif_file, format="GTiff", overwrite=TRUE)
+  ie <- writeRaster(raster_map, filename=tif_file, format="GTiff", overwrite=TRUE)
 }
+
+#
 
